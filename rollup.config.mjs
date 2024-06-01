e2d3d5fd6846e33964c3c9b4709336e5675b317b
John F Morton <morton@jmx2.com>
2024-06-01 11:34:56 -0400

    working on es module build options

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,34 +3,44 @@ import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 
 export default [
-    // unpkg
-    {
-        input: 'src/index.ts',
-        output: {
-            file: 'dist/p5.svg.js',
-            format: 'iife',
-            sourcemap: true
-        },
-        plugins: [typescript(), resolve(), commonjs()]
+  // unpkg
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/p5.svg.js',
+      format: 'iife',
+      sourcemap: true
     },
-    // cjs (webpack, vite)
-    {
-        input: 'src/index.ts',
-        output: {
-            file: 'dist/p5.svg.cjs.js',
-            format: 'cjs',
-            sourcemap: true
-        },
-        plugins: [typescript(), resolve(), commonjs()]
+    plugins: [typescript(), resolve(), commonjs()]
+  },
+  // cjs (webpack, vite)
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/p5.svg.cjs.js',
+      format: 'cjs',
+      sourcemap: true
     },
-    // test
-    {
-        input: 'test/unit/index.js',
-        output: {
-            file: 'dist/test.js',
-            format: 'iife',
-            sourcemap: true
-        },
-        plugins: [resolve(), commonjs()]
-    }
+    plugins: [typescript(), resolve(), commonjs()]
+  },
+  /// esm (vite)
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/p5.svg.esm.js',
+      format: 'esm',
+      sourcemap: true
+    },
+    plugins: [typescript(), resolve(), commonjs()]
+  },
+  // test
+  {
+    input: 'test/unit/index.js',
+    output: {
+      file: 'dist/test.js',
+      format: 'iife',
+      sourcemap: true
+    },
+    plugins: [resolve(), commonjs()]
+  }
 ]
